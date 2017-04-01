11a5ff595ca1ab91c82f8967daeeae4a268ff5cb
Halisson Vitor <halissonvit@gmail.com>
2017-04-01 17:43:04 -0300

    setup rspec and capybara

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+feature 'my home page' do
+    scenario 'hello world message' do
+        #Use capybara to auto complete forms and navigate
+        visit('/')
+        expect(page).to have_content("Hello World!")
+    end
+end  
